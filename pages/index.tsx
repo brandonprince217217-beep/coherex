c064f66e2748eb7f7b2510dd8ab3a4682d33cb25
brandonprince217217-beep <brandonprince217217@gmail.com>
2026-02-23 14:40:58 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,9 @@
-index.tsx
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>Coherex</h1>
+      <p>A minimal Next.js project deployed from your iPhone.</p>
+    </div>
+  )
+}
+
