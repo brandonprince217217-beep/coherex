227518f776ec8e3aee13978e462de7b92bd8887a
brandonprince217217-beep <brandonprince217217@gmail.com>
2026-02-24 17:55:54 -0600

    Wrap navigation in header element

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,11 @@
 export default function Header() {
   return (
-    <nav className="flex flex-col items-start gap-6 p-6 text-2xl">
-      <a href="/" className="hover:text-white">Home</a>
-      <a href="/about" className="hover:text-white">About</a>
-      <a href="/pricing" className="hover:text-white">Pricing</a>
-    </nav>
+    <header>
+      <nav className="flex flex-col items-start gap-6 p-6 text-2xl">
+        <a href="/" className="hover:text-white">Home</a>
+        <a href="/about" className="hover:text-white">About</a>
+        <a href="/pricing" className="hover:text-white">Pricing</a>
+      </nav>
+    </header>
   );
 }
