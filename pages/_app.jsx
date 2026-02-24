0a76099832d2fb0b4c6c94bbab77c4ef0bec78e8
brandonprince217217-beep <263283324+brandonprince217217-beep@users.noreply.github.com>
2026-02-24 11:52:10 -0600

    Fix app layout

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,24 @@
 import '../styles/globals.css';
 
-function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+export default function App({ Component, pageProps }) {
+  return (
+    <div className="min-h-screen bg-black text-white">
 
-export default App;
+      {/* GLOBAL NAV */}
+      <nav className="flex space-x-6 items-center p-6">
+        <a href="/" className="hover:text-gray-300 transition">Home</a>
+        <a href="/about" className="hover:text-gray-300 transition">About</a>
+        <a
+          href="/pricing"
+          className="px-4 py-2 rounded-lg bg-gradient-to-r from-blue-600 to-purple-600 font-semibold hover:opacity-90 transition"
+        >
+          Pricing
+        </a>
+      </nav>
+
+      {/* PAGE CONTENT */}
+      <Component {...pageProps} />
+
+    </div>
+  );
+}
