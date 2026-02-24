c042df5fa9706695b53077cda8a152b738938861
brandonprince217217-beep <brandonprince217217@gmail.com>
2026-02-23 19:01:07 -0600

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,13 @@
 import Header from './Header';
+import Footer from './Footer';
 
 export default function Layout({ children }) {
   return (
     <>
       <Header />
       <main>{children}</main>
+      <Footer />
     </>
   );
 }
+
