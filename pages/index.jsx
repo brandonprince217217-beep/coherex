6f877401ce17cf99418ee8877f510f8696f89441
brandonprince217217-beep <brandonprince217217@gmail.com>
2026-02-24 10:40:05 -0600

    Refactor index.jsx to remove nav and add About component
    Removed navigation links and added About component.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,7 @@
-<nav className="flex space-x-6 items-center text-white">
-  <a href="/" className="hover:text-gray-300 transition">Home</a>
-  <a href="/about" className="hover:text-gray-300 transition">About</a>
-
-  <a
-    href="/pricing"
-    className="px-4 py-2 rounded-lg bg-gradient-to-r from-blue-600 to-purple-600 font-semibold hover:opacity-90 transition"
-  >
-    Pricing
-  </a>
-</nav>
+export default function About() {
+  return (
+    <div>
+      {/* your content */}
+    </div>
+  );
+}
