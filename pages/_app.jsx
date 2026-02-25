e571327108cf6ea53dd8d12c63c5e8510d9a3065
brandonprince217217-beep <brandonprince217217@gmail.com>
2026-02-25 11:54:50 -0600

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,9 @@
-import '../styles/globals.css';
-import '../styles/home.css';
-import '../styles/sections.css';
+import Layout from "../components/Layout";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
-
-export default MyApp;
