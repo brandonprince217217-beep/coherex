a5ffa8781161165d11c321f2032d551a0ec5c8a6
brandonprince217217-beep <brandonprince217217@gmail.com>
2026-02-25 15:09:35 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, no-cache, must-revalidate, proxy-revalidate"
+          },
+          {
+            key: "Pragma",
+            value: "no-cache"
+          },
+          {
+            key: "Expires",
+            value: "0"
+          }
+        ]
+      }
+    ];
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
