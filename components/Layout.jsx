892fe3b574a4ad5dff2ce7944e51fee5f47f5715
brandonprince217217-beep <brandonprince217217@gmail.com>
2026-02-25 17:44:45 -0600

    Remove background color from Layout
    Remove background color from Layout component.

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,7 +2,6 @@ export default function Layout({ children }) {
   return (
     <div
       style={{
-        background: "#0a0a0a",
         minHeight: "100vh",
         color: "white",
         fontFamily: "Inter, sans-serif",
