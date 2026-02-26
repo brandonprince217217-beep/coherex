ecfa8083a475d26ab1074aeb355e1821222dc0f0
brandonprince217217-beep <brandonprince217217@gmail.com>
2026-02-26 14:33:54 -0600

    Integrate SearchBar into Home component
    Added a SearchBar component to the Home page.

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import Constellation from '../components/Constellation';
 import Vortex from '../components/Vortex';
 import HoloCore from '../components/HoloCore';
 import Layout from '../components/Layout';
+import SearchBar from '../components/SearchBar';
 
 export default function Home() {
   return (
@@ -12,55 +13,66 @@ export default function Home() {
         <meta name="description" content="Cognitive OS" />
       </Head>
 
-      <div style={{
-        position: 'relative',
-        width: '100%',
-        height: '100vh',
-        overflow: 'hidden',
-        backgroundColor: 'black'
-      }}>
-        
+      <div
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '100vh',
+          overflow: 'hidden',
+          backgroundColor: 'black'
+        }}
+      >
         {/* Background Constellation */}
-        <div style={{
-          position: 'absolute',
-          inset: 0,
-          zIndex: 1
-        }}>
+        <div
+          style={{
+            position: 'absolute',
+            inset: 0,
+            zIndex: 1
+          }}
+        >
           <Constellation />
         </div>
 
         {/* Vortex Layer */}
-        <div style={{
-          position: 'absolute',
-          inset: 0,
-          zIndex: 2
-        }}>
+        <div
+          style={{
+            position: 'absolute',
+            inset: 0,
+            zIndex: 2
+          }}
+        >
           <Vortex />
         </div>
 
         {/* HoloCore (center engine) */}
-        <div style={{
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          zIndex: 3
-        }}>
+        <div
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            zIndex: 3
+          }}
+        >
           <HoloCore />
         </div>
 
         {/* Main Content */}
-        <div style={{
-          position: 'relative',
-          zIndex: 4,
-          color: 'white',
-          textAlign: 'center',
-          marginTop: '40vh'
-        }}>
+        <div
+          style={{
+            position: 'relative',
+            zIndex: 4,
+            color: 'white',
+            textAlign: 'center',
+            marginTop: '40vh'
+          }}
+        >
           <h1>Coherex Cognitive OS</h1>
           <p>Welcome to your cognitive engine.</p>
-        </div>
 
+          {/* SEARCH BAR ADDED HERE */}
+          <SearchBar />
+        </div>
       </div>
     </Layout>
   );
