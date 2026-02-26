9f4b96c1c873f2911f0bc21aa4b670ae0cb38dcf
brandonprince217217-beep <brandonprince217217@gmail.com>
2026-02-26 14:54:45 -0600

    Update index.jsx

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -5,17 +5,28 @@ export default function About() {
     <Layout>
       <div
         style={{
-          position: 'absolute',
-          top: '75%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          color: 'white',
-          textAlign: 'center',
-          zIndex: 20
+          position: 'relative',
+          width: '100%',
+          height: '100vh',
+          backgroundColor: 'black',
+          overflow: 'hidden',
+          zIndex: 5
         }}
       >
-        <h1>About Page</h1>
-        <p>This is the About page.</p>
+        <div
+          style={{
+            position: 'absolute',
+            top: '75%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            color: 'white',
+            textAlign: 'center',
+            zIndex: 20
+          }}
+        >
+          <h1>About Page</h1>
+          <p>This is the About page.</p>
+        </div>
       </div>
     </Layout>
   );
