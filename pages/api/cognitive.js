d3d356fbd10c5c7420ba01c0b09ffaadf395244e
brandonprince217217-beep <brandonprince217217@gmail.com>
2026-02-27 10:12:08 -0600

    Add TypeScript types to API handler
    Added TypeScript types for request and response parameters.

--- pages/api/cognitive.js
+++ pages/api/cognitive.js
@@ -1,7 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from "next";
 import { openai } from "../../lib/openai";
 import { emptyAnalysis } from "../../lib/cognitive/model";
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
