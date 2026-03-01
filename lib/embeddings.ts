4b3c423f7a554f1a3e03d59aa3f4361d38953482
Coherex <brandonprince217217@gmail.com>
2026-03-01 11:55:32 -0600

    Add embedding and cosine functions in embeddings.ts

---
+++ lib/embeddings.ts
@@ -0,0 +1,24 @@
+import { openai } from "./openai";
+
+export async function embed(text: string): Promise<number[]> {
+  const res = await openai.embeddings.create({
+    model: "text-embedding-3-small",
+    input: text,
+  });
+
+  return res.data[0].embedding;
+}
+
+export function cosine(a: number[], b: number[]) {
+  let dot = 0;
+  let na = 0;
+  let nb = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    dot += a[i] * b[i];
+    na += a[i] * a[i];
+    nb += b[i] * b[i];
+  }
+
+  return dot / (Math.sqrt(na) * Math.sqrt(nb));
+}
