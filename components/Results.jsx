08a602a1bdf05c462484829a463f228aed4bdb0e
Coherex <brandonprince217217@gmail.com>
2026-03-01 14:00:31 -0600

    Refactor Results component to simplify props

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,19 +1,11 @@
 import React from "react";
 import ResultCard from "./ResultCard";
 
-export interface CoherexResult {
-  answer: string;
-}
-
-interface Props {
-  results: CoherexResult[];
-}
-
-export default function Results({ results }: Props) {
+export default function Results({ results }) {
   return (
     <div className="flex flex-col items-center mt-10 space-y-8">
-      {results.map((r, i) => (
-        <ResultCard key={i} answer={r.answer} />
+      {results.map((item, index) => (
+        <ResultCard key={index} answer={item.answer} />
       ))}
     </div>
   );
